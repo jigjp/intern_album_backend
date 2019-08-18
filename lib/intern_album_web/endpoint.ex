69f12871f84747f2db2d028a94f1c2e41c287e40
yamada <yamada@jig.jp>
2019-08-18 18:28:12 +0900

    🔧 SPAをserveできるように

--- lib/intern_album_web/endpoint.ex
+++ lib/intern_album_web/endpoint.ex
@@ -13,7 +13,7 @@ defmodule InternAlbumWeb.Endpoint do
     at: "/",
     from: :intern_album,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images statics index.html js favicon.ico robots.txt)
 
   plug Plug.Static, at: "/media", from: "media/"
 
